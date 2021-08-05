03e03969c9f21e8f67a91766308ff9a5acc66747
Beck0797 <bekhzod.abduraupov@mail.ru>
2021-08-05 23:56:17 +0900

    Repository

--- app/src/main/java/com/beck/architectureexample/NoteDatabase.java
+++ app/src/main/java/com/beck/architectureexample/NoteDatabase.java
@@ -1,10 +1,14 @@
 package com.beck.architectureexample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
+import androidx.loader.content.AsyncTaskLoader;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -21,8 +25,35 @@ public abstract class NoteDatabase extends RoomDatabase {
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
         //this function will delete DB with all the tables and creates it from scratch
+                    .addCallback(roomCallBack)
                     .build();
         }
         return instance;
     }
+
+        private static RoomDatabase.Callback roomCallBack = new RoomDatabase.Callback(){
+            @Override
+            public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                super.onCreate(db);
+
+                new PopulateDbAsyncTask(instance).execute();
+            }
+        };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db){
+            noteDao = db.noteDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 2));
+            noteDao.insert(new Note("Title 3", "Description 3", 3));
+
+            return null;
+        }
+    }
+
 }
